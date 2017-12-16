878f0ebe0f7cea844f381c63602df90acddf4392
starfighter-ix <ec14370@qmul.ac.uk>
2017-12-16 00:09:41 +0000

    Final commit.

--- query.js
+++ query.js
@@ -18,14 +18,48 @@ db.employees.find( { $and: [ { dob: { $gt: new Date("1990") } }, { sex: { $eq: "
 // Female employees born after 1989, with their age rounded down and contact details shown, sorted by ascending age.
 db.employees.aggregate( [ { $match: { $and: [ { dob: { $gt: new Date("1990") } }, { sex: { $eq: "F" } } ] } }, { $sort: { "dob": -1 } }, { $project: { _id: 0, first_name: 1, last_name: 1, "age": { $floor : { $divide: [ { $subtract: [ new Date(), "$dob" ] }, (1000 * 86400 * 365 ) ] } }, email: 1, phone: 1 }  } ] );
 
-// No. of drivers working for each shift period in a given month.
+// No. of drivers working for each shift period in a given month. (Experimental)
 db.shifts.aggregate( [ { $unwind: "$on_duty" }, { $lookup: { from: "drivers" , localField: "on_duty.employee_id", foreignField: "employee_id", as: "driver_docs"} }, { $project: { date: 1, period: 1, on_duty: 1, "driver_docs": { $filter: {  input: "$driver_docs", as: "driver_doc", cond: { $eq: [ "$$driver_doc.employee_id", "on_duty.employee_id" ] } } } } } ] );
+
+
+// (Experimental)
+db.drivers.aggregate( [ 
+	{ $lookup: 
+	  { 
+	    from: "shifts" ,
+	    localField: "employee_id",
+	    foreignField: "on_duty", 
+	    as: "shift_docs"
+	   } 
+	},
+	{ $unwind: "$shift_docs.on_duty" },
+	{ $project: 
+	  { 
+	    date: 1,
+	    period: 1,
+	    on_duty: 1,
+	    "shift_docs": 
+	    	{ $filter: 
+	    	  {  input: "$shift_docs", 
+         	    as: "shift_doc", 
+	     	    cond: 
+	     	    { $eq: [ 
+	     	      "$$shift_doc.on_duty.employee_id", 
+	     	      "employee_id"
+	     	      ] 
+	     	    } 
+	     	   } 
+	     	} 
+	    } 
+	} 
+]);
 
 // Compute total of all overhead costs for a given document. 
 db.overheadCosts.aggregate( [ { $project: { total: { $sum: [ "$lighting", "$heating", "$fuel"] } } } ] )
 
-// CAUTION: Not fully implemented!
+// CAUTION: Not fully implemented! (Experimental)
 db.shifts.aggregate( [ { $unwind: "$on_duty" }, { $lookup: { from: "employees", localField: "on_duty.employee_id", foreignField: "_id", as: "employee_docs" } }, { $project: { period: 1 } }  ] ) 
+
 
 // Group employees by cities.
 db.employees.aggregate( [ 
